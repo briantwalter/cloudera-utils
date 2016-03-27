4f71d1ee4b42a3697d366a49b69ab08f7fea2a22
briantwalter <walter.brian@gmail.com>
2016-03-26 20:43:41 -0700

    another

--- bootstrap-bwalter-post.sh
+++ bootstrap-bwalter-post.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "...Starting Custom Post-Bootstrap"
+
+sudo adduser -u 1501 -p cloudera brian
+sudo adduser -u 1501 -p cloudera oryx
+
 echo "...Ending Custom Post-Bootstrap"
 exit 0
