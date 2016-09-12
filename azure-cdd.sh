eb0f12e469787bb2e078e9041757d82fdbfc0e6a
briantwalter <walter.brian@gmail.com>
2016-09-12 12:04:53 -0700

    a3

--- azure-cdd.sh
+++ azure-cdd.sh
@@ -1,19 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "INFO: Preparing Azure instance for Cloudera Director"
 echo "INFO: Installing packages"
 sudo yum -y update
 sudo yum -y install git curl wget vim python-pip screen mariadb bind bind-utils
 
-# Timezone change if you want it?
-#cp -p /etc/sysconfig/clock /etc/sysconfig/clock.orig
-#sed -e 's/New_York/Los_Angeles/' /etc/sysconfig/clock.orig > /etc/sysconfig/clock
-#mv /etc/localtime /etc/localtime.orig
-#ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-
 echo "INFO: Installing Java 8 JDK from Oracle"
-URL=http://download.oracle.com/otn-pub/java/jdk/8u101-b13
-RPM=jdk-8u101-linux-x64.rpm
+URL="http://download.oracle.com/otn-pub/java/jdk/8u101-b13"
+RPM="jdk-8u101-linux-x64.rpm"
 curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" ${URL}/${RPM} > /tmp/${RPM}
 sudo rpm -i /tmp/${RPM}
 
@@ -26,5 +20,4 @@ sudo yum -y install cloudera-director-server cloudera-director-client
 echo "INFO: Starting Cloudera Director"
 sudo service cloudera-director-server start
 
-echo "...Ending Azure instance prep"
-exit 0
+echo "INFO: Ending Azure instance prep"
