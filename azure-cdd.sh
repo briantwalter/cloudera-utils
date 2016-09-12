9edd94b553164ed4c6e9e8a7f25a7546c41627ea
briantwalter <walter.brian@gmail.com>
2016-09-12 11:54:08 -0700

    a2

--- azure-cdd.sh
+++ azure-cdd.sh
@@ -2,7 +2,8 @@
 
 echo "INFO: Preparing Azure instance for Cloudera Director"
 echo "INFO: Installing packages"
-yum -y update; yum -y install git curl wget vim python-pip screen mariadb bind bind-utils
+sudo yum -y update
+sudo yum -y install git curl wget vim python-pip screen mariadb bind bind-utils
 
 # Timezone change if you want it?
 #cp -p /etc/sysconfig/clock /etc/sysconfig/clock.orig
@@ -10,7 +11,7 @@ yum -y update; yum -y install git curl wget vim python-pip screen mariadb bind b
 #mv /etc/localtime /etc/localtime.orig
 #ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-echo "INFO: Installing Java 8 JDK from Oracle
+echo "INFO: Installing Java 8 JDK from Oracle"
 URL=http://download.oracle.com/otn-pub/java/jdk/8u101-b13
 RPM=jdk-8u101-linux-x64.rpm
 curl -v -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" ${URL}/${RPM} > /tmp/${RPM}
@@ -19,7 +20,8 @@ sudo rpm -i /tmp/${RPM}
 echo "INFO: Configuring Cloudera Director YUM Repo"
 sudo wget "http://archive.cloudera.com/director/redhat/7/x86_64/director/cloudera-director.repo" -O /etc/yum.repos.d/cloudera-director.repo
 echo "INFO: Installing Cloudera Director from RPMs"
-sudo yum -y update; sudo yum -y install cloudera-director-server cloudera-director-client
+sudo yum -y update
+sudo yum -y install cloudera-director-server cloudera-director-client
 
 echo "INFO: Starting Cloudera Director"
 sudo service cloudera-director-server start
